11fa8907d30e3f9cdfbb7bae1ca07a46d4b30d40
LeoMcBills <leokinyera9@gmail.com>
2024-01-31 22:45:40 +0300

    class

--- basics.py
+++ basics.py
@@ -5,7 +5,36 @@ print(numbers[1])
 def add(x, y):
     return x + y
 
-
 print(add(2, 2))
 print(add(2, 3))
 print(add(4, 5))
+
+
+# 12 + 12.4
+# print(type(z))
+
+'''
+12'/0'+'/0'12.4
+
+['12', '+', '12.4']
+
+'''
+
+class Summer:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def adder(self):
+        return self.x + self.y
+    
+
+add1 = Summer(12, 12.4)
+
+print(add1.adder())
+
+
+
+
+
